3fcfcb69f7a7e23c20e078c10de301c977af3086
arjunsabu300 <arjunsabumark32@gmail.com>
2025-06-16 23:02:38 +0530

    finalcommitsss

--- Pick_Your_Poison/src/Backend/app.js
+++ Pick_Your_Poison/src/Backend/app.js
@@ -58,5 +58,23 @@ app.post('/api/questions',async(req,res)=>{
     }
 })
 
+
+app.get('/healthcheck', (req, res) => {
+  res.status(200).send('OK');
+});
+
+
+
+
+setInterval(() => {
+  axios.get('https://notedown-project.onrender.com/healthcheck')
+    .then(response => {
+      console.log('Ping successful:', response.data);
+    })
+    .catch(error => {
+      console.error('Ping failed:', error.message);
+    });
+}, 5 * 60 * 1000); 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
